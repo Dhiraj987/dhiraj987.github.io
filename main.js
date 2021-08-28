11bca50f5c1a3bbb5279d85becb947ce7af78c18
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-08-28 01:02:42 -0400

    contact info

--- main.js
+++ main.js
@@ -4,4 +4,6 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("for_sticky", window.scrollY > 0)
+
+    header.classList.toggle("nav_bar_with_shadow", this.window.scrollY > 740)
 })
